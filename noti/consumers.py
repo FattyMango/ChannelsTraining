33317575a4ed3a5cff30da1e71ce97f6a6840ffa
fattymango <malak.jkl@gmial.com>
2021-03-02 21:52:37 +0200

    pushing noti

--- noti/consumers.py
+++ noti/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer,WebsocketConsu
 from channels.db import database_sync_to_async
 from .models import notification
 from django.contrib.auth.models import User
+
 class noticonsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         
@@ -19,62 +20,15 @@ class noticonsumer(AsyncJsonWebsocketConsumer):
         data = content.get('content',None)
         userto = content.get('to_email',None)
         userfrom = content.get('from_email',None)
-        print((data,userto,userfrom))
+        
         noti = await create_noti(userfrom=userfrom,userto=userto,content = data)
-        print(noti)
+        
         await self.send_json(content=json.dumps({
-            'content' : noti.content,
-            'from' : noti.sender.username
+            'content' : str(noti.content),
+            'from' : str(noti.sender.username),
+            'time' : str(noti.date_created)
         }))
         
-
-# class noticonsumer(WebsocketConsumer):
-#     def connect(self):
-        
-#         print('Connected')
-#         # Join room group
-#         self.accept()
-
-#     def disconnect(self, close_code):
-#         print('Disconnected')
-
-#     # Receive message from WebSocket
-#     def receive(self,text_data):
-#         content = json.loads(text_data)
-#         data = content['content']
-#         userto = content['to_email']
-#         userfrom = content['from_email']
-#         print((data,userto,userfrom))
-#         noti = create_noti(userfrom=userfrom,userto=userto,content = data)
-#         print(noti)
-#         self.send(text_data=json.dumps({
-#             'content' : noti.content,
-#             'from' : noti.sender.username
-#         }))        
-
-# class noticonsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-        
-#         print('Connected')
-#         # Join room group
-#         await self.accept()
-
-#     async def disconnect(self, close_code):
-#         print('Disconnected')
-
-#     # Receive message from WebSocket
-#     async def receive(self,text_data):
-#         content = json.loads(text_data)
-#         data = content['content']
-#         userto = content['to_email']
-#         userfrom = content['from_email']
-#         print((data,userto,userfrom))
-#         noti = await create_noti(userfrom=userfrom,userto=userto,content = data)
-#         print(noti)
-#         await self.send(text_data=json.dumps({
-#             'content' : noti.content,
-#             'from' : noti.sender.username
-#         })) 
 @database_sync_to_async           
 def  create_noti (userfrom,userto,content):
     print('create_noti called')
--- noti/models.py
+++ noti/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 class notification (models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='sender',null = False,blank=False)
     reciever = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='reciever',null = False,blank=False)
-    content = models.CharField(max_length=100,null= False,blank=False)    
+    content = models.CharField(max_length=100,null= False,blank=False)
+    date_created = models.DateField(auto_now_add=True,null=True)    
     def __str__(self):
         return f'({self.reciever}) : {self.content}'
