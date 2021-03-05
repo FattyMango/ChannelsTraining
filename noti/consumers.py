5ad91bd2e57204fcffd2440a1627ec7ba502c7be
fattymango <malak.jkl@gmial.com>
2021-03-06 00:18:37 +0200

    improving the message system by only using consumers

--- noti/consumers.py
+++ noti/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import notification
 from django.contrib.auth.models import User
+from post.models import *
 class notiConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope['user']
@@ -26,10 +27,11 @@ class notiConsumer(AsyncWebsocketConsumer):
         message = text_data_json['message']
         sender = text_data_json['sender']
         reciever = text_data_json['reciever']
-        noti = await create_noti( userfrom=sender,userto=reciever,content=message)
+        postpk = text_data_json['postpk']
+        noti = await create_noti( userfrom=sender,userto=reciever,content=message,postpk = postpk)
         await self.channel_layer.group_send(
 
-            self.group_name,{
+            str(noti.reciever.pk),{
                'type':'send_message',
                'message':message,
                'username' : noti.sender.username,
@@ -44,9 +46,13 @@ class notiConsumer(AsyncWebsocketConsumer):
         await self.send(text_data= json.dumps({'message':message,'username':username}))
         
 @database_sync_to_async           
-def  create_noti (userfrom,userto,content):
+def  create_noti (userfrom,userto,content,postpk):
     
     user1 = User.objects.get(email=userto)
     user2 = User.objects.get(email=userfrom)
-    
+    post = Post.objects.get(id = postpk)
+    try:
+        Like.objects.get(user=user1,post=post)
+    except Like.DoesNotExist:
+        like = Like.objects.create(user=user1,post=post)
     return notification.objects.create(reciever=user1,sender=user2,content=content)
